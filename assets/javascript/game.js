126c4ffcf400af7a2a126061033a0565bc22d5c4
Tiur Mawaddah <tiurzm@gmail.com>
2019-09-28 10:10:21 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,30 @@
 $(document).ready(function(){
-        $(".image").on("click", function(){
-            $(".image").off("click");
-            $(".my-char").append($(this));
-            $(".characters>.image").each(function(){
-                $(this).removeClass("image").addClass("my-image");
-                $(".enemies").append($(this));
-            });
-            $(".characters").remove();
-            $(".my-image").on("click", function(){
-                $(".other").append($(this));
-                $(this).addClass("defender");
-            });
-    
+    var counter = 0; 
+
+    // choose a character
+    $(".image").on("click", function(){
+        $(".image").off("click");
+        $(this).css("background", "white");
+        $(".my-char").append($(this));
+        $(".characters>.image").each(function(){
+            $(this).removeClass("image").addClass("my-image");
+            $(this).css("background", "#ff944d");
+            $(".enemies").append($(this));
+        });
+        $(".characters").remove();
+        // choose an enemy
+        $(".my-image").on("click", function(){
+            $(this).css("background","black")
+            $(this).css("color","white")
+            $(".defender").append($(this));
+            console.log(this);
+            // attack the enemy 
+            $(".attack").on("click", function(){
+                var newH = $("<h3>");
+                newH.addClass("clear");
+                newH.text("You attacked your enemy");
+                $(".defender").append(newH);
+            }); 
+        });
     });
-    
 }); 
