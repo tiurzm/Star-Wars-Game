8d9752229f758ec6202bb3e9ddc970aca9838343
Tiur Mawaddah <tiurzm@gmail.com>
2019-09-28 19:18:05 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     var counter = 0; 
-
+    var obi = 120;
+    var luke = 100;
+    var sidious = 150;
+    var maul = 180; 
     // choose a character
     $(".image").on("click", function(){
         $(".image").off("click");
@@ -8,23 +11,26 @@ $(document).ready(function(){
         $(".my-char").append($(this));
         $(".characters>.image").each(function(){
             $(this).removeClass("image").addClass("my-image");
-            $(this).css("background", "#ff944d");
             $(".enemies").append($(this));
         });
         $(".characters").remove();
+
         // choose an enemy
         $(".my-image").on("click", function(){
             $(this).css("background","black")
             $(this).css("color","white")
             $(".defender").append($(this));
             console.log(this);
-            // attack the enemy 
+            // button attack the enemy 
             $(".attack").on("click", function(){
                 var newH = $("<h3>");
+                var text = $(".defender .tag").text()
                 newH.addClass("clear");
-                newH.text("You attacked your enemy");
+                newH.html("You attacked " + text + " for" + " demage"+ "<br>" + text + " attacked you back for" + " demage" + "</br>");
                 $(".defender").append(newH);
             }); 
         });
+
+        
     });
 }); 
