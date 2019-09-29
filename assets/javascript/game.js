51e8994375e767939c46f7d86df2dee70e499a11
Tiur Mawaddah <tiurzm@gmail.com>
2019-09-29 13:00:51 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,49 @@
 $(document).ready(function(){
-    var counter = 0; 
-
     var characters = {
-        obi: {
-            hitPoints: 120,
-            attackPoints: 8,
-        },
-        luke: {
-            hitPoints: 100,
-            attackPoints: 5,
-        },
-        sidious: {
-            hitPoints: 150,
-            attackPoints: 20,
-        },
-        maul: {
-            hitPoints: 180,
-            attackPoints: 25,
+        obi: {hitPoints: 120, attackPoints: 8},
+        luke: {hitPoints: 100,attackPoints: 5},
+        sidious: {hitPoints: 150, attackPoints: 20},
+        maul: {hitPoints: 180,attackPoints: 25}
+    }
+
+    var attacker= "obi";
+    for (var i = 0; i < characters.length; i++){
+        if (attacker === characters[i]) {
+            alert("hai");
         }
     }
-    console.log(characters);
-   
+
+    $(".image").attr(characters);
 
     // choose a character
     $(".image").on("click", function(){
         $(".image").off("click");
         $(this).css("background", "white");
         $(".my-char").append($(this));
-        $(".characters>.image").each(function(){
+
+        // move to enemies
+        $("#characters>.image").each(function(){
             $(this).removeClass("image").addClass("my-image");
             $(".enemies").append($(this));
         });
-        $(".characters").remove();
-
+        $("#characters").remove();
+        
         // choose an enemy
         $(".my-image").on("click", function(){
             $(this).css("background","black")
             $(this).css("color","white")
             $(".defender").append($(this));
-            console.log(this);
             // button attack the enemy 
             $(".attack").on("click", function(){
                 var newH = $("<h3>");
-                var text = $(".defender .tag").text()
+                var text = $(".defender .tag").text();
                 newH.addClass("clear");
-                newH.html("You attacked " + text + " for" + " demage"+ "<br>" + text + " attacked you back for" + " demage" + "</br>");
+                newH.html("You attacked " + text + " for " + " demage"+ "<br>" + text + " attacked you back for" + " demage" + "</br>");
                 $(".defender").append(newH);
+
                 // attack
                 $(".attack").on("click", function(){
-                    alert("hei");
-              
+    
                 }); 
             }); 
         });
